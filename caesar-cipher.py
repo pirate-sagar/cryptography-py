5b48e95dcf639b9c37f733211c9b94bdbb87dc36
Pirate Sagar <u2piratesagar@gmail.com>
2024-12-31 07:21:42 +0530

    caesar cipher v1 completed

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,1 +1,26 @@
-print("this is caesar cipher")
+alphabets="+=~`''""<>,.?/{}[]|\!@#$%^&*()_-1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabetsObj = {}
+for i in range(0,len(alphabets)):
+    alphabetsObj[alphabets[i]] = i
+
+userText = input("enter text: ")
+
+userText = userText.replace(" ","_")
+
+userTextObj=[]
+
+for i in range(0, len(userText)):
+        userTextObj.append([userText[i],alphabetsObj[userText[i]]])
+
+key=2
+
+for i in range(0, len(userTextObj)):
+        userTextObj[i][1]+=key
+
+encryptedText=""
+
+for i in userTextObj:
+    key = next((k for k, v in alphabetsObj.items() if v == i[1]), None)
+    encryptedText+=key
+
+print("encrypted text",encryptedText)
